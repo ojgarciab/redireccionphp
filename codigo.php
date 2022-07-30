6f2f9ecf7a4e6d5e7ecc08a657840cb1259d20d7
Óscar García <ojgarciab@users.noreply.github.com>
2022-07-30 23:17:39 +0200

    Errata al limpiar código

--- codigo.php
+++ codigo.php
@@ -1,7 +1,7 @@
 <?php
 /* Procura evitar lógica inversa y, a ser posible, el operador fusión de null */
 switch (
-    substr($_SERVER['HTTP_ACCEPT_LANGUAGE']) ?? 'es', 0, 2)
+    substr($_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? 'es', 0, 2)
 ) {
     //El error es que si un usuario cuyo idioma es castellano visita la raiz, es redirigido nuevamente aunque YA ESTÉ en la raiz. La idea es que quizá con una cookie detecte si ya está en la raiz y no haga la redirección para evitar el error de "TOO MANY REDIRECTS".
     case 'es':
