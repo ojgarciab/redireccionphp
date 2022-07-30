66696d5c7cae71cbb806c7351daa9abbd38dec6c
Óscar García <ojgarciab@users.noreply.github.com>
2022-07-30 22:06:47 +0200

    Simplificación del código

--- codigo.php
+++ codigo.php
@@ -1,26 +1,20 @@
 <?php
-//Este código está hasta arriba de mi index.php.
-//Y el  idioma principal es castellano
-$accept_languaje = !empty($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : 'es';
-$main_languaje   = substr($accept_languaje, 0, 2);
-//tanto el index, como "en.php" y "pt.php" están en la raiz del dominio
-switch ($main_languaje):
+/* Procura evitar lógica inversa y, a ser posible, el operador fusión de null */
+switch (substr(
+    $_SERVER['HTTP_ACCEPT_LANGUAGE']) ?? 'es',
+    0,
+    2
+)) {
     //El error es que si un usuario cuyo idioma es castellano visita la raiz, es redirigido nuevamente aunque YA ESTÉ en la raiz. La idea es que quizá con una cookie detecte si ya está en la raiz y no haga la redirección para evitar el error de "TOO MANY REDIRECTS".
     case 'es':
-        exit(header("Location: https://ejemplo.com/"));
-    case 'en':
-        exit(header("Location: https://ejemplo.com/en.php"));
+        /* No tenemos que redirigir a ningún sitio */
+        break;
     case 'pt':
-        exit(header("Location: https://ejemplo.com/pt.php"));
-endswitch;
-if (false !== strpos($accept_languaje, 'es')) {
-    exit(header("Location: https://ejemplo.com/"));
+        header("Location: https://ejemplo.com/pt.php");
+        exit();
+        break;
+    default:
+        /* Ante cualquier otro idioma que no sea "es" o "pt" asumimos el inglés */
+        header("Location: https://ejemplo.com/en.php");
+        exit();
 }
-if (false !== strpos($accept_languaje, 'en')) {
-    exit(header("Location: https://ejemplo.com/en.php"));
-}
-if (false !== strpos($accept_languaje, 'pt')) {
-    exit(header("Location: https://ejemplo.com/pt.php"));
-}
-//Si el usuario habla otro idioma, se redirecciona a la versión en Inglés
-exit(header("Location: https://ejemplo.com/en.php"));
