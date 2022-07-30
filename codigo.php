9f462a1ce7ffa9db2cae2473960f51db11c75dfc
Óscar García <ojgarciab@users.noreply.github.com>
2022-07-30 22:12:20 +0200

    Separo la función del switch

--- codigo.php
+++ codigo.php
@@ -1,10 +1,12 @@
 <?php
 /* Procura evitar lógica inversa y, a ser posible, el operador fusión de null */
-switch (substr(
-    $_SERVER['HTTP_ACCEPT_LANGUAGE']) ?? 'es',
-    0,
-    2
-)) {
+switch (
+    substr(
+        $_SERVER['HTTP_ACCEPT_LANGUAGE']) ?? 'es',
+        0,
+        2
+    )
+) {
     //El error es que si un usuario cuyo idioma es castellano visita la raiz, es redirigido nuevamente aunque YA ESTÉ en la raiz. La idea es que quizá con una cookie detecte si ya está en la raiz y no haga la redirección para evitar el error de "TOO MANY REDIRECTS".
     case 'es':
         /* No tenemos que redirigir a ningún sitio */
